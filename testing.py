4ef72f7428a19183734dca98c10ee04e5e6e9662
bharath <bharathwajraom@gmail.com>
2026-05-07 19:32:11 +0530

    hello

--- testing.py
+++ testing.py
@@ -1,7 +1,7 @@
-a=input("enter a word ")
-for i in a:
-    if (i=="A" or i=="a"):
-        print("a is found")
-        break
-    else:
-        print("a is not found")
+var=10
+while var>0:
+    var=var-1
+    if var==5:
+        continue
+    print(var)
+print("goodbye")
