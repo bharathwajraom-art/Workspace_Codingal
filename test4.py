258f8b9d6f763c72280f1ce4624a46ee245e0bb2
bharathwajraom-art <bharathwajraom@gmail.com>
2026-06-18 19:36:00 +0530

    Create test4.py

---
+++ test4.py
@@ -0,0 +1,7 @@
+class Vehicle:
+    def __init__ (self,maxspeed,mileage):
+        self.maxspeed=maxspeed
+        self.mileage=mileage
+m=Vehicle(200,18)
+print("ModelMax Speed:",m.maxspeed)
+print("Modelmileage Speed:",m.mileage)
